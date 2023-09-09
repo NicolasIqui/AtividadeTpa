dfd044236a73705b738a196e9a1b2a05f7e2dbf8
NicolasIqui <142914085+NicolasIqui@users.noreply.github.com>
2023-09-09 01:55:18 -0300

    Update ExercicioFibonacci.java

--- ExercicioFibonacci.java
+++ ExercicioFibonacci.java
@@ -2,12 +2,12 @@ public class ExercicioFibonacci {
     public static void main(String[] args) {
     
         int num=1,numa=0,nump=num+numa;
-        while(num<55){
+        while(num<=55){
             nump=numa+num;
             numa=num;
             num=nump;
 
-             System.out.println("o numero e "+nump );
+             System.out.println("o numero e "+numa );
         }
         
     }
